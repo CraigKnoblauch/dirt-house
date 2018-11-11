2f5e147337b103d951dcef6e2645054d7122430d
Craig Knoblauch <craigknoblauch@gmail.com>
2018-11-11 13:58:38 -0700

    start of navigation module

---
+++ Squirt/nav.lua
@@ -0,0 +1,20 @@
+squirt = require("robot")
+debug = require("component").debug
+
+--[[ Serves as a wrapper for a robot's forward function. Without this function, squirt would 
+    report his position as the block he came from, rather than the block he's going to. ]]
+function sqForward(debug)
+    -- Get the coordinates before the move
+    local initX, initY, initZ
+    initX = debug.getX()
+    initY = debug.getY()
+    initZ = debug.getZ()
+
+    -- While squirt has not moved relative to his starting location
+    while initX == debug.getX() and initY == debug.getY() and initZ == debug.getZ() do
+        -- Block
+    end
+
+    -- Return the current position. 
+    return debug.getX(), debug.getY(), debug.getZ()
+end
