607436d303b118f1aced72ca919233dbfa40920c
Craig Knoblauch <craigknoblauch@gmail.com>
2018-11-11 15:57:52 -0700

    updated as per advice of Vexatos

--- Squirt/nav.lua
+++ Squirt/nav.lua
@@ -1,25 +1,25 @@
-squirt = require("robot")
-debug = require("component").debug
+local squirt = require("robot")
+local debug = require("component").debug
 
 -- Table of functions to return
 local nav = {}
 
 --[[ Serves as a wrapper for a robot's forward function. Without this function, squirt would 
     report his position as the block he came from, rather than the block he's going to. ]]
-function nav.sqForward(debug)
+local function nav.sqForward()
     -- Get the coordinates before the move
     local initX, initY, initZ
-    initX = debug.getX()
+    initX = debug.getX() - 1
     initY = debug.getY()
     initZ = debug.getZ()
 
     -- While squirt has not moved relative to his starting location
-    while initX == debug.getX() and initY == debug.getY() and initZ == debug.getZ() do
+    while initX == debug.getX() - 1 and initY == debug.getY() and initZ == debug.getZ() do
         -- Block
     end
 
     -- Return the current position. 
-    return debug.getX(), debug.getY(), debug.getZ()
+    return debug.getX() - 1, debug.getY(), debug.getZ()
 end
 
 return nav
