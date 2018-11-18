88a172e80c3df94f648fc00bb37c600a95b24306
Craig Knoblauch <craigknoblauch@gmail.com>
2018-11-18 02:02:36 -0700

    back, up, down

--- Squirt/sq-swim.lua
+++ Squirt/sq-swim.lua
@@ -30,5 +30,71 @@ function swim.sqForward()
     return ret
 end
 
+--[[ Serves as a wrapper for a robot's back function. Without this function, squirt would 
+    report his position as the block he came from, rather than the block he's going to. ]]
+function swim.sqBack()
+    -- Get the coordinates before the move
+    local initX, initY, initZ
+    initX, initY, initZ = sq_nav.sqGetPos()
+    
+    -- Move squirt back
+    ret = squirt.back()
+    if ret == true then
+
+        -- While squirt has not moved relative to his starting location
+        while initX == sq_nav.sqGetX() and initY == sq_nav.sqGetY() and initZ == sq_nav.sqGetZ() do
+            -- Block
+        end
+
+    end
+
+    -- Return the back's usual return
+    return ret
+end
+
+--[[ Serves as a wrapper for a robot's up function. Without this function, squirt would 
+    report his position as the block he came from, rather than the block he's going to. ]]
+function swim.sqUp()
+    -- Get the coordinates before the move
+    local initX, initY, initZ
+    initX, initY, initZ = sq_nav.sqGetPos()
+    
+    -- Move squirt up
+    ret = squirt.up()
+    if ret == true then
+
+        -- While squirt has not moved relative to his starting location
+        while initX == sq_nav.sqGetX() and initY == sq_nav.sqGetY() and initZ == sq_nav.sqGetZ() do
+            -- Block
+        end
+
+    end
+
+    -- Return the up's usual return
+    return ret
+end
+
+--[[ Serves as a wrapper for a robot's down function. Without this function, squirt would 
+    report his position as the block he came from, rather than the block he's going to. ]]
+function swim.sqDown()
+    -- Get the coordinates before the move
+    local initX, initY, initZ
+    initX, initY, initZ = sq_nav.sqGetPos()
+    
+    -- Move squirt down
+    ret = squirt.down()
+    if ret == true then
+
+        -- While squirt has not moved relative to his starting location
+        while initX == sq_nav.sqGetX() and initY == sq_nav.sqGetY() and initZ == sq_nav.sqGetZ() do
+            -- Block
+        end
+
+    end
+
+    -- Return the down's usual return
+    return ret
+end
+
 return swim
     
