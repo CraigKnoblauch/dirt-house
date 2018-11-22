5f8f1a08425739e143b8ac457fcf31b6385275a9
Craig Knoblauch <craigknoblauch@gmail.com>
2018-11-21 20:32:15 -0700

    top block name functionality; needs test

--- Squirt/sq-act.lua
+++ Squirt/sq-act.lua
@@ -280,6 +280,44 @@ function act.sqGetLeftBlockName()
     return block
 end
 
+--[[ Local function to only be used with functions in sq-act module.
+    Returns the coordinates of one block on top of squirt ]]
+local function getTopPos()
+
+    -- Get Squirt's current position
+    local squirtX, squirtY, squirtZ
+    squirtX, squirtY, squirtZ = sq_nav.sqGetPos()
+
+    -- Make copies of squirt's postion to be edited
+    local blockX, blockY, blockZ = squirtX, squirtY, squirtZ
+
+    -- Determine which direction Squirt is facing
+    local facing = sq_swim.sqGetFacing()
+
+    -- Increment Y by 1
+    blockY = blockY + 1
+
+    return blockX, blockY, blockZ
+end
+        
+--[[ Return the name of the block on top of squirt. Only blocks in the
+    local getBlockName function are supported. ]]
+function act.sqGetTopBlockName()
+
+    -- Get the coordinate of the block on top of Squirt
+    local topX, topY, topZ
+    topX, topY, topZ = getTopPos()
+
+    -- Get the block id and meta data with world object
+    local id = world.getBlockId(topX, topY, topZ)
+    local meta = world.getMetadata(topX, topY, topZ)
+
+    -- Get the block name on top of squirt
+    local block = getBlockName(id, meta)
+
+    return block
+end
+
 return act
     
 
