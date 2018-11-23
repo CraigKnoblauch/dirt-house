406cef6b637cd87aaf640709ad8d94c6b5e61cef
Craig Knoblauch <craigknoblauch@gmail.com>
2018-11-22 21:37:22 -0700

    comms connect for test

--- Squirt/sq-comms.lua
+++ Squirt/sq-comms.lua
@@ -3,6 +3,7 @@
 local squirt = require("robot")
 local debug = require("component").debug
 local nav = require("component").navigation
+local it = require("component").internet
 local sq_nav = require("sq-navigation")
 local sq_swim = require("sq-swim")
 local sq_act = require("sq-act")
@@ -10,6 +11,15 @@ local sq_act = require("sq-act")
 -- Table to hold functions of this module we wish the caller to have
 local comms = {}
 
+-- The ip address that Squirt must connect to
+local ADDRESS = "127.0.0.1"
 
+-- The port that Squirt must connect to
+local PORT = 65432
+
+--[[ Returns a handle to the connection opened at ADDRESS:PORT ]]
+function comms.sqConnect()
+    return it.connect(ADDRESS, PORT)
+end
 
 return comms
